2de46d97548a422f765903d1d7ae48f936da7769
frontalNugity <nuge@geniusfactor.ca>
2018-07-28 00:29:58 -0700

    Shell script for installing extensions

---
+++ install_extensions.sh
@@ -0,0 +1,23 @@
+# Configures a default install of VSCODe with the prescibed themes and
+# extensions suitable for Laravel Full-stack development
+
+# Ted Nugent (nuge@geniusfactor.ca) - July 2018
+
+# Extensions for full-stack development
+code --install-extension mrmlnc.vscode-scss
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension onecentlin.laravel-blade
+code --install-extension peterjausovec.vscode-docker
+code --install-extension tyriar.sort-lines
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension mikestead.dotenv
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension cssho.vscode-svgviewer
+code --install-extension felixfbecker.php-debug
+code --install-extension gruntfuggly.scoper
+code --install-extension ikappas.composer
+code --install-extension ikappas.phpcs
+code --install-extension knisterpeter.vscode-github
+code --install-extension jock.svg
+code --install-extension wwm.better-align
