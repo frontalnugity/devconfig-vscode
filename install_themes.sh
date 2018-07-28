d3185e394d1b3850ed3085db8c4b68929a58e5b4
frontalNugity <nuge@geniusfactor.ca>
2018-07-28 00:30:39 -0700

    Collection of high-q themes

---
+++ install_themes.sh
@@ -0,0 +1,18 @@
+# Configures a default install of VSCODe with the prescibed themes and
+# Recommened themes suitable for Laravel Full-stack development
+# Ted Nugent (nuge@geniusfactor.ca) - July 2018
+
+code --install-extension ahmadawais.shades-of-purple
+code --install-extension alireza94.theme-gotham
+code --install-extension cooproton.folabelle
+code --install-extension kube.theme-kay
+code --install-extension legfrey.rocket-ui
+code --install-extension leif-niemczik.delta-color-theme
+code --install-extension lorceroth.theme-crucible
+code --install-extension max-ss.cyberpunk
+code --install-extension oscarmcm.ocean-space
+code --install-extension sdras.night-owl
+code --install-extension sveggiani.vscode-field-lights
+code --install-extension thenikso.github-plus-theme
+code --install-extension tyriar.theme-sapphire
+code --install-extension yummygum.city-lights-theme
