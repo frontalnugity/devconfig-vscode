51b67d575d4f38333979c36f9cf346e689cd5aab
frontalNugity <nuge@geniusfactor.ca>
2018-07-28 00:30:08 -0700

    Shell script for installing icons

---
+++ install_icons.sh
@@ -0,0 +1,6 @@
+# Configures a default install of VSCODe with the prescibed themes and
+# Recommended Icon packages suitable for Laravel Full-stack development
+# Ted Nugent (nuge@geniusfactor.ca) - July 2018
+
+code --install-extension pkief.material-icon-theme
+code --install-extension yummygum.city-lights-icon-vsc
