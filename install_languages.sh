52f30c6ea448b4b4ac87d493879a642cf2f13e09
frontalNugity <nuge@geniusfactor.ca>
2018-07-28 01:10:54 -0700

    Languages for Laravel

--- install_languages.sh
+++ install_languages.sh
@@ -1,29 +1,7 @@
 # Configures a default install of VSCODe with the prescibed themes and
 # extensions suitable for Laravel Full-stack development
-
 # Ted Nugent (nuge@geniusfactor.ca) - July 2018
 
-
-# Geneeral editor and usability extensions
-
-code --install-extension tyriar.sort-lines
-code --install-extension wayou.vscode-todo-highlight
-code --install-extension coenraads.bracket-pair-colorizer
-code --install-extension gruntfuggly.scoper
-code --install-extension wwm.better-align
-
-# Extensions primarily for PHP
-code --install-extension ikappas.phpcs
-code --install-extension neilbrayfield.php-docblocker
-code --install-extension bmewburn.vscode-intelephense-client
-code --install-extension felixfbecker.php-debug
-
-code --install-extension mrmlnc.vscode-scss
-code --install-extension peterjausovec.vscode-docker
-code --install-extension ikappas.composer
-code --install-extension knisterpeter.vscode-github
-
-code --install-extension cssho.vscode-svgviewer
 
 # Syntaxes & Languages
 code --install-extension jock.svg
